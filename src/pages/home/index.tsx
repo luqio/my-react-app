64f8e903cafdbca612a6a370e0f276dd6362afd9
qilu <oqilu@163.com>
2025-04-02 18:52:45 +0800

    feat:调试公式

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import IconInternet from '@/assets/svg/icon_internet.svg?react';
 import styles from  './index.module.less';
 
@@ -6,13 +6,19 @@ import styles from  './index.module.less';
 function Home() {
   // 使用useState钩子，定义一个名为count的状态变量，初始值为0
   const [count, setCount] = useState(0);
+  const domRef = useRef(null);
 
+  useEffect(() => {
+    (window as any).katex.render("$\\Delta T_{\\text{华氏}} \\approx 0.054 \\, ^\\circ\\text{F}$", domRef.current, {
+      throwOnError: false
+  });
+  }, [])
   // 返回一个JSX元素，包含一个TestLint组件，三个div元素，两个a元素，一个h1元素，一个div元素，一个p元素，一个p元素
   return (
     <>
       <div>
         <br />
-        <span className={styles.icon}>
+        <span className={styles.icon} ref={domRef}>
           <IconInternet className="tc-svg-icon" />
         </span>
       </div>
